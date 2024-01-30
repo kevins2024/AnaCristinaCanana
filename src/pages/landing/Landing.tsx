1701eaa5d0aa7b22170283af2e1a49f0816f7457
Kevin Smith <kevins2024@outlook.com>
2024-01-30 15:41:19 +0000

    landing page layout and alignment finished. needs refactoring and css variables at least, plus looks terrible in mobile

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -4,8 +4,26 @@ function Landing() {
   return (
     <div className="landing-content" style={{ color: "white" }}>
       <div className="landing-content-center-bar">
-        <div>BIG NAME</div>
-        <div>little words</div>
+        <div className="landing-content-name">ANA CRISTINA CANANA</div>
+        <div
+          className="landing-content-profession"
+          style={{ color: "#B9B9B9" }}
+        >
+          <div style={{ flex: 1, minWidth: "30%" }}>
+            <div style={{ float: "right" }}>
+              <span className="landing-content-profession-left-box">
+                JOURNALIST
+              </span>
+              <span className="landing-content-profession-left-box">
+                PROFESSOR
+              </span>
+            </div>
+          </div>
+          <div className="landing-content-profession-center-box">
+            UX DESIGNER
+          </div>
+          <div style={{ flex: 1, minWidth: "30%" }}></div>
+        </div>
       </div>
     </div>
   );
--- src/shared/components/TopBar.tsx
+++ src/shared/components/TopBar.tsx
@@ -1,16 +1,20 @@
 import "./TopBar.css";
 import acLogo from "../../assets/logo-ac-black.png";
-import { useState } from "react";
 
 function TopBar() {
   return (
     <div className="top-bar">
       <img
         src={acLogo}
-        style={{ width: "42px", height: "50px", paddingLeft: "20px" }}
+        style={{ width: "42px", height: "50px", marginLeft: "20px" }}
       />
       <div className="top-bar-button-box">
-        <div style={{ color: "white" }}>this is the right stuff</div>
+        {/* change these to links */}
+        <div className="top-bar-button">PROJECTS</div>
+        <div className="top-bar-button">LOGOS</div>
+        <div className="top-bar-button">CHARACTERS</div>
+        <div className="top-bar-button">ABOUT</div>
+        <div className="top-bar-button">PHOTOGRAPHY</div>
       </div>
     </div>
   );
