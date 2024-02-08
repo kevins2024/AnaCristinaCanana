009e71a6a4f9ad6c0ace8822576197d253625df6
Kevin Smith <kevins2024@outlook.com>
2024-02-08 10:21:31 +0000

    setting up

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Routes, Route } from "react-router";
 import "./App.css";
 import Landing from "./pages/landing/Landing";
-import TopBar from "./shared/components/TopBar";
+import TopBar from "./components/TopBar";
 import Photography from "./pages/photography/Photography";
 import Logos from "./pages/logos/Logos";
+import Projects from "./pages/projects/Projects";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/logos" element={<Logos />} />
         <Route path="/photography" element={<Photography />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </div>
   );
---
+++ src/pages/projects/Projects.tsx
@@ -0,0 +1,13 @@
+import "./Projects.css";
+
+function Projects() {
+  return (
+    <div className="projects-content content-panel">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+    </div>
+  );
+}
+
+export default Projects;
