dae257e02eb0be1057a6ac7440999ebdcebe8af1
Kevin Smith <kevins2024@outlook.com>
2024-03-15 13:49:44 +0000

    trying to link up with DNS

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -4,8 +4,14 @@ import taqueriaImage from "./assets/taqueria-preview.png";
 import meetImage from "./assets/meet-preview.png";
 import orbImage from "./assets/orb-preview.png";
 import { ProjectNames, ProjectText } from "./ProjectText";
+import { useNavigate } from "react-router-dom";
 
 function Projects() {
+  const navigate = useNavigate();
+
+  const handleClick = (uri: string) => {
+    navigate(uri);
+  };
   return (
     <div className="projects-content content-panel">
       {/* <ProjectPreview
@@ -23,22 +29,39 @@ function Projects() {
         text={ProjectText[ProjectNames.ORB]}
         url="/projects/myorb"
       /> */}
-      <div style={{ display: "flex", flexDirection: "column", width: "100%" }}>
-        <div style={{ height: "10vh" }}></div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "90%",
+          justifyContent: "center",
+        }}
+      >
         <div style={{ display: "flex", justifyContent: "space-around" }}>
-          <img src={taqueriaImage} style={{ width: "25vw" }} />
-          <img src={meetImage} style={{ width: "25vw" }} />
-          <img src={orbImage} style={{ width: "25vw" }} />
+          <button
+            onClick={() => handleClick("/projects/taqueria")}
+            style={{
+              border: "none",
+              backgroundColor: "white",
+              cursor: "pointer",
+            }}
+          >
+            <img src={taqueriaImage} style={{ width: "25vw" }} />
+          </button>
+          <img src={meetImage} style={{ width: "25vw", cursor: "pointer" }} />
+          <img src={orbImage} style={{ width: "25vw", cursor: "pointer" }} />
         </div>
         <div style={{ height: "4vh" }}></div>
         <div style={{ display: "flex", justifyContent: "space-around" }}>
-          <span style={{ width: "25vw" }}>
+          <span style={{ width: "25vw", textAlign: "center" }}>
             {ProjectText[ProjectNames.TAQUERIA]}
           </span>
-          <span style={{ width: "25vw" }}>
+          <span style={{ width: "25vw", textAlign: "center" }}>
             {ProjectText[ProjectNames.MEET]}
           </span>
-          <span style={{ width: "25vw" }}>{ProjectText[ProjectNames.ORB]}</span>
+          <span style={{ width: "25vw", textAlign: "center" }}>
+            {ProjectText[ProjectNames.ORB]}
+          </span>
         </div>
         <div style={{ height: "10vh" }}></div>
       </div>
