881d39e03b65259aab9ea33f71dd05dd72adc52a
Kevin Smith <kevins2024@outlook.com>
2024-03-22 16:03:52 +0000

    css for paper collection

--- src/pages/characters/Characters.tsx
+++ src/pages/characters/Characters.tsx
@@ -13,24 +13,30 @@ function Characters() {
         {
           "When I worked as a preschool teacher I would find myself enjoying children's books more than my students. I love the graphics, the silly/funny stories and all the creativity involved in them. When I got an iPad I started to play with Procreate and Illustrator. That's how I gave life to all my 'kids'! My goal one day is to publish and illustrate my very own children's book. Meanwhile, I have been designing paper collections for use in crafting in collaboration with My Enchanted Colors in Mexico."
         }
+        <div style={{ height: "4vh" }}></div>
         <div style={{ textAlign: "left" }} className="project-content-title">
           Paper Collection
         </div>
         <br />
         <div
           style={{
-            display: "grid",
-            gridTemplateColumns: "30% 30% 30%",
-            gap: "5%",
+            display: "flex",
+            justifyContent: "space-around",
           }}
         >
-          <img style={{ maxWidth: "100%" }} src={image2} />
-          <img style={{ maxWidth: "100%" }} src={image3} />
-          <img style={{ maxWidth: "100%" }} src={image4} />
+          <div>
+            <img style={{ maxWidth: "80%" }} src={image2} />
+          </div>
+          <div>
+            <img style={{ maxWidth: "80%" }} src={image3} />
+          </div>
+          <div>
+            <img style={{ maxWidth: "80%" }} src={image4} />
+          </div>
         </div>
       </div>
       <br />
-      <div style={{ minHeight: "40vh", visibility: "hidden" }}>hidden</div>
+      <div style={{ minHeight: "20vh", visibility: "hidden" }}>hidden</div>
     </div>
   );
 }
