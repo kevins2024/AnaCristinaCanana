cf78d135f4bc71ca02d72e9236ce6d368944fa1e
Kevin Smith <kevins2024@outlook.com>
2024-05-02 12:05:57 +0100

    rework of projects page, hopefully last time

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -1,110 +1,27 @@
 import "./Projects.css";
-import taqueriaImage from "./assets/taqueria-preview.png";
-import meetImage from "./assets/meet-preview.png";
-import orbImage from "./assets/orb-preview.png";
-import vetImage from "./assets/pocket-vet-preview.png";
-import { ProjectNames, ProjectText } from "./ProjectText";
-import { useNavigate } from "react-router-dom";
+
+import { ProjectsData } from "./ProjectData";
+import Project from "./Project";
 
 function Projects() {
-  const navigate = useNavigate();
-  const hrefTaqueria = "/projects/taqueria";
-  const hrefMeet = "/projects/meet";
-  const hrefOrb = "/projects/myorb";
-  const hrefVet = "/project/pocketvet";
-
-  function handleClick(url: string) {
-    navigate(url);
-  }
-
   return (
-    // todo: is there another way to lay these out? or programmatically add items from a list?
     <div className="projects-content content-panel">
       <div className="projects-layout">
         {/* todo: make a component for spacer that accepts the height */}
-        <div className="flex-spacer" style={{ height: "12vh" }}></div>
-        <div className="project-preview-row">
-          <button
-            className="button-image project-preview-row-item"
-            onClick={() => handleClick(hrefOrb)}
-          >
-            <img src={orbImage} className="project-preview-row-item-inner" />
-          </button>
-          <button
-            className="button-image project-preview-row-item"
-            onClick={() => handleClick(hrefTaqueria)}
-          >
-            <img
-              src={taqueriaImage}
-              className="project-preview-row-item-inner"
-            />
-          </button>
-          <button
-            className="button-image project-preview-row-item"
-            onClick={() => handleClick(hrefMeet)}
-          >
-            <img src={meetImage} className="project-preview-row-item-inner" />
-          </button>
-        </div>
-        <div className="flex-spacer" style={{ height: "2vh" }}></div>
-        <div className="project-preview-row">
-          <span className="project-preview-row-item">
-            {ProjectText[ProjectNames.VET]}
-          </span>
-          <span className="project-preview-row-item">
-            {ProjectText[ProjectNames.TAQUERIA]}
-          </span>
-          <span className="project-preview-row-item">
-            {ProjectText[ProjectNames.ORB]}
-          </span>
-        </div>
-        <div className="flex-spacer" style={{ height: "2vh" }}></div>
-        <div className="project-preview-row">
-          <button
-            className="project-preview-row-button primary-button"
-            onClick={() => handleClick(hrefOrb)}
-          >
-            Read More
-          </button>
-          <button
-            className="project-preview-row-button primary-button"
-            onClick={() => handleClick(hrefTaqueria)}
-          >
-            Read More
-          </button>
-          <button
-            className="project-preview-row-item primary-button"
-            onClick={() => handleClick(hrefOrb)}
-          >
-            Read More
-          </button>
-        </div>
-        <div className="flex-spacer" style={{ height: "4vh" }}></div>
-        <div className="project-preview-row">
-          <button
-            className="button-image"
-            onClick={() => handleClick(hrefMeet)}
-          >
-            <img src={meetImage} className="project-preview-row-item" />
-          </button>
-        </div>
-        <div style={{ height: "2vh" }}></div>
-        <div className="project-preview-row">
-          <span className="project-preview-row-item">
-            {ProjectText[ProjectNames.MEET]}
-          </span>
-        </div>
-        <div className="flex-spacer" style={{ height: "2vh" }}></div>
-        <div className="project-preview-row">
-          <button
-            className="project-preview-row-button primary-button"
-            onClick={() => handleClick(hrefMeet)}
-          >
-            Read More
-          </button>
-        </div>
-        <div className="flex-spacer" style={{ height: "28vh" }}></div>
+        {/* todo: enforce the order */}
+        {ProjectsData.map((proj) => (
+          <Project
+            name={proj.name}
+            image={proj.image}
+            text={proj.text}
+            url={proj.url}
+          />
+        ))}
       </div>
+      <div
+        className="vertical-spacer"
+        style={{ minHeight: "10vh", width: "100px" }}
+      ></div>
     </div>
   );
 }
